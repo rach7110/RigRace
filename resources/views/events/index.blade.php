327bef05cc73e02aeb65a0ea002093990ceacf55
Rachel <rachel.loziuk@gmail.com>
2017-07-10 20:00:54 -0500

    add 'New' button to Event index page

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -3,7 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-2 pull-right">
+            <a href="{{route('events.create')}}">
+                <button class="btn btn-primary">New Event</button>
+            </a>
+        </div>
+    </div>
+
+    </br>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">My Events</div>
 
