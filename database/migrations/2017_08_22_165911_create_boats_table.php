c32c16656985a84fa4599321661b5e2df02dfba5
Rachel <rachel.loziuk@gmail.com>
2017-08-22 12:09:03 -0500

    create Boat model and migration

---
+++ database/migrations/2017_08_22_165911_create_boats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBoatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('result_id');
+            $table->string('boatname');
+            $table->string('skipper_first');
+            $table->string('skipper_last');
+            $table->integer('rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boats');
+    }
+}
