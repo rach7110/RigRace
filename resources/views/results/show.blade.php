8972fda78372d52c9a583a4c4b9b9ec7428377dd
Rachel <rachel.loziuk@gmail.com>
2017-08-22 11:19:58 -0500

    add edit and delete links to result show page

--- resources/views/results/show.blade.php
+++ resources/views/results/show.blade.php
@@ -42,6 +42,25 @@
                         <label>User:</label>
                         {{ $user->id}}
                     </div>
+
+
+                    <div class="row">
+                        <!-- EDIT -->
+                        <div class="col-md-1">
+                            <a href="{{ route('results.edit', $result->id) }}">Edit</a>
+                        </div>
+                        <!-- DELETE -->
+                        <form method="post" action="{{route('results.destroy', $result->id)}}">
+                            {{ csrf_field() }}
+                            <button class="btn btn-danger btn-link" type="submit" style="padding-top: 0px; border-top-width: 0px;">Delete</button>
+                            <input type="hidden" name="_method" value="DELETE">
+                        </form>
+                    </div>
+
+                    <div>
+                    </div>
+                    <div>
+                    </div>
                 </div>
             </div>
         </div>
