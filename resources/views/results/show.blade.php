ace0ca94a40ab8aa25bdb264c15cbebb3e9e01d6
Rachel <rachel.loziuk@gmail.com>
2017-08-23 16:52:04 -0500

    display more boat info on result page

--- resources/views/results/show.blade.php
+++ resources/views/results/show.blade.php
@@ -22,7 +22,9 @@
     </div>
     </br>
     <div class="panel panel-default">
-        <div class="panel-heading">{{$result->eventname}}</div>
+        <div class="panel-heading">
+            <h3 class="panel-title">{{$result->eventname}}</h3>
+        </div>
         <div class="panel-body">
             <div>
                 <label>Host:</label>
@@ -57,16 +59,28 @@
 
     <!-- BOATS -->
     <div class="panel panel-default">
-        <div class="panel-heading">Boats</div>
+        <div class="panel-heading">
+            <h3 class="panel-title">Boats</h3>
+        </div>
         <div class="panel-body">
             @if(count($boats) == 0)
                 <p>No competing boats. Click 'Add Boat' to start scoring.</p>
             @else
-                <ul>
+                <div class="row">
+                    <div class="col-md-2">Boat Name</div>
+                    <div class="col-md-2">Boat Rating</div>
+                    <div class="col-md-2">Skipper First Name</div>
+                    <div class="col-md-2">Skipper Last Name</div>
+                </div>
                     @foreach ($boats as $boat)
-                        <li>{{ $boat->boatname }}</li>
+                        <div class="row">
+                            <div class="col-md-2">{{ $boat->boatname }}</div>
+                            <div class="col-md-2">{{ $boat->rating}}</div>
+                            <div class="col-md-2">{{ $boat->skipper_first}}</div>
+                            <div class="col-md-2">{{ $boat->skipper_last}}</div>
+                        </div>
                     @endforeach
-                </ul>
+                </div>
             @endif
         </div>
 
