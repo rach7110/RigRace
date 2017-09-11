37caa51c15f35b4953bc5ad7a75ee8d3284139c2
Rachel <rachel.loziuk@gmail.com>
2017-09-11 10:50:54 -0400

    create migration for races table

---
+++ database/migrations/2017_09_11_141850_create_races_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('races', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('result_id');
+            $table->integer('number');
+            $table->decimal('distance',7,4)->nullable();
+            $table->dateTime('start_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('races');
+    }
+}
