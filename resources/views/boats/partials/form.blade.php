866257b125a846a0d25a08e9a1f1663e3e800a68
Rachel <rachel.loziuk@gmail.com>
2017-09-13 14:58:03 -0400

    add placeholder values to forms

--- resources/views/boats/partials/form.blade.php
+++ resources/views/boats/partials/form.blade.php
@@ -3,27 +3,28 @@
     <div class="form-group">
         <label for="boatname" class="col-sm-2 control-label">Boat Name</label>
         <div class="col-sm-10">
-            <input type="text" name="boatname" id="boatname" class="form-control" placeholder="" value="@if(isset($boat)) {{ $boat->boatname }} @else {{ old('boatname') }} @endif ">
+            <input type="text" name="boatname" id="boatname" class="form-control" value="@if(isset($boat->boatname)) {{ $boat->boatname }} @elseif(old('boatname')) {{ old('boatname') }} @endif" placeholder="Blue Bayou" required>
         </div>
     </div>
     <!-- BOAT RATING -->
     <div class="form-group">
         <label for="rating" class="col-sm-2 control-label">Boat Rating</label>
         <div class="col-sm-10">
-            <input type="number" name="rating" class="form-control" id="rating" value="@if(isset($boat)){{$boat->rating}}@else {{ old('rating') }} @endif">
+            <input type="number" name="rating" class="form-control" id="rating" value="@if(isset($boat->rating)){{ $boat->rating}}@elseif(old('rating')) {{ old('rating') }} @endif" placeholder="102">
         </div>
     </div>
     <!-- SKIPPER -->
     <div class="form-group">
         <label for="skipper_first" class="col-sm-2 control-label">Skipper First Name</label>
         <div class="col-sm-10">
-            <input type="text" name="skipper_first" class="form-control" id="skipper_first" placeholder="" value="@if(isset($boat)) {{ $boat->skipper_first }} @else {{ old('skipper_first') }} @endif "/>
+            <input type="text" name="skipper_first" class="form-control" id="skipper_first" value="@if(isset($boat->skipper_first)) {{ $boat->skipper_first }} @elseif(old('skipper_first')) {{ old('skipper_first') }} @endif" placeholder="Kristen" />
         </div>
     </div>
     <div class="form-group">
         <label for="skipper_last" class="col-sm-2 control-label">Skipper Last Name</label>
         <div class="col-sm-10">
-            <input type="text" name="skipper_last" class="form-control" id="skipper_last" placeholder="" value="@if(isset($boat)) {{ $boat->skipper_last }} @else {{ old('skipper_last') }} @endif "/>
+            <input type="text" name="skipper_last" class="form-control" id="skipper_last" value="@if(isset($boat->skipper_last)) {{ $boat->skipper_last }} @elseif(old('skipper_last')) {{ old('skipper_last') }} @endif" placeholder="Fasten" />
+
         </div>
     </div>
 
--- resources/views/results/partials/form.blade.php
+++ resources/views/results/partials/form.blade.php
@@ -3,28 +3,28 @@
     <div class="form-group">
         <label for="eventname" class="col-sm-2 control-label">Event Name</label>
         <div class="col-sm-10">
-            <input type="text" name="eventname" id="eventname" class="form-control" placeholder="2017 Summer Series Races" value="@if(isset($result)) {{ $result->eventname }} @else {{ old('eventname') }} @endif ">
+            <input type="text" name="eventname" id="eventname" class="form-control" value="@if(isset($result->eventname)) {{ $result->eventname }} @elseif(old('eventname')) {{ old('eventname') }} @endif" placeholder="Summer Series Races" >
         </div>
     </div>
     <!-- HOST -->
     <div class="form-group">
         <label for="venue" class="col-sm-2 control-label">Venue</label>
         <div class="col-sm-10">
-            <input type="text" name="venue" class="form-control" id="venue" placeholder="Windy Yacht Club" value="@if(isset($result)) {{ $result->venue }} @else {{ old('venue') }} @endif "/>
+            <input type="text" name="venue" class="form-control" id="venue" value="@if(isset($result->host)) {{ $result->host }} @elseif(old('host')) {{ old('host') }} @endif" placeholder="Windy Yacht Club" />
         </div>
     </div>
     <!-- START DATE -->
     <div class="form-group">
         <label for="start_date" class="col-sm-2 control-label">Start Date</label>
         <div class="col-sm-10">
-            <input type="date" name="start_date" class="form-control" id="start_date" value="@if(isset($result)){{$result->start_date}}@else {{ old('start_date') }} @endif">
+            <input type="date" name="start_date" class="form-control" id="start_date" value="@if(isset($result->start_date)){{$result->start_date}}@elseif(old('start_date')) {{ old('start_date') }} @endif">
         </div>
     </div>
     <!-- END DATE -->
     <div class="form-group">
         <label for="end_date" class="col-sm-2 control-label">End Date</label>
         <div class="col-sm-10">
-            <input type="date" name="end_date" class="form-control" id="end_date" value="@if(isset($result)){{$result->end_date}}@else {{ old('end_date') }} @endif">
+            <input type="date" name="end_date" class="form-control" id="end_date" value="@if(isset($result->end_date)){{$result->end_date}}@else(old('end_date')){{ old('end_date') }} @endif">
         </div>
     </div>
     
